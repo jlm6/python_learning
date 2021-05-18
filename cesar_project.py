1d6441d332ddb07a8ddb812fa09c792847370159
jlm6 <84381833+jlm6@users.noreply.github.com>
2021-05-18 20:46:00 +0800

    Update cesar_project.py

--- cesar_project.py
+++ cesar_project.py
@@ -25,10 +25,8 @@ while not end_game:
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
   text = input("Type your message:\n").lower()
   shift = int(input("Type the shift number:\n"))
-
   if shift > 26:
     shift %= 26
-
 
   caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
   selection = input("Do you want to restart the game?!(yes/no)").lower()
